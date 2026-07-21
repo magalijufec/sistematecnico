0465ad23d15a82716d48f86879d9504dba266517
magalijufec <magali.medina@jufecsa.com.ar>
2026-07-21 19:13:05 -0300

    se arreglo usuarios list

--- src/app/features/usuarios/usuarios-list/usuarios-list.ts
+++ src/app/features/usuarios/usuarios-list/usuarios-list.ts
@@ -1,10 +1,25 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { UsuarioService } from '../../../core/services/usuario.service';
 import { Usuario } from '../../../core/models/usuario';
-
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 @Component({
   selector: 'app-usuarios-list',
   standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatTableModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule
+  ],
   templateUrl: './usuarios-list.html',
   styleUrl: './usuarios-list.scss'
 })
@@ -13,6 +28,15 @@ export class UsuariosListComponent implements OnInit {
   private usuarioService = inject(UsuarioService);
 
   usuarios: Usuario[] = [];
+  displayedColumns = [
+  'userName',
+  'nombreApellido',
+  'perfil',
+  'provincia',
+  'ciudad',
+  'activo',
+  'acciones'
+];
 
   ngOnInit(): void {
     this.cargarUsuarios();
