6d5e42856ab01b9c3b5336b479f04228078f5acd
magalijufec <magali.medina@jufecsa.com.ar>
2026-07-23 11:38:25 -0300

    se modifico para que sea responsive

--- src/app/shared/layouts/main-layout/main-layout.ts
+++ src/app/shared/layouts/main-layout/main-layout.ts
@@ -1,32 +1,75 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
+import {
+  RouterOutlet,
+  RouterLink,
+  RouterLinkActive
+} from '@angular/router';
+
+import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
-import { MENU } from '../../../core/constants/menu';
-import { RouterLinkActive } from '@angular/router';
+
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { ViewChild, inject } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
+
+import { MENU } from '../../../core/constants/menu';
+
 @Component({
   selector: 'app-main-layout',
   standalone: true,
   imports: [
     RouterOutlet,
     RouterLink,
+    RouterLinkActive,
+
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatListModule,
-    RouterLinkActive
+    MatListModule
   ],
   templateUrl: './main-layout.html',
   styleUrls: ['./main-layout.scss']
 })
-export class MainLayoutComponent {
-protected menu = MENU;
+export class MainLayoutComponent implements OnInit {
+
+  protected menu = MENU;
+
+  @ViewChild('drawer')
+  drawer!: MatSidenav;
+
+  private breakpointObserver = inject(BreakpointObserver);
+
+  esMovil = false;
+
+  ngOnInit(): void {
+
+    this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe(result => {
+
+        this.esMovil = result.matches;
+
+      });
+
+  }
+
+  toggleMenu(): void {
+
+    this.drawer.toggle();
+
+  }
+
+  navegar(): void {
+
+    // En celular cerramos el menú
+    if (this.esMovil) {
+
+      this.drawer.close();
+
+    }
+
+  }
 
 }
