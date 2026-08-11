323cc1ee9c67657e86d72f7c61a2ba2fcf033833
magalijufec <magali.medina@jufecsa.com.ar>
2026-08-11 08:41:31 -0300

    fix dashboard

--- src/app/core/models/dashboard.ts
+++ src/app/core/models/dashboard.ts
@@ -1,21 +1,14 @@
 export interface Dashboard {
-
   pendientes: number;
-
   enProceso: number;
-
+  trabajosFinalizados: number;
+  aprobados: number;
   pendientePago: number;
-
   finalizados: number;
 
   totalTrabajos: number;
-
   totalClientes: number;
-
   totalTecnicos: number;
-
   trabajosHoy: number;
-
   trabajosMes: number;
-
 }
