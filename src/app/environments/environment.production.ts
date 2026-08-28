2f79d7fe6ad6293755e0a121823a98ae74150f95
magalijufec <magali.medina@jufecsa.com.ar>
2026-08-28 08:01:37 -0300

    fix url environment

--- src/app/environments/environment.production.ts
+++ src/app/environments/environment.production.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true, 
     environmentName: 'production', 
     apiUrl: 'http://192.168.0.143/sistemaTecnicoApi/api',
-    api: 'http://192.168.0.143'
+    api: 'http://192.168.0.143/sistemaTecnicoApi'
 };
--- src/app/environments/environment.testing.ts
+++ src/app/environments/environment.testing.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false, 
     environmentName: 'testing', 
     apiUrl: 'http://192.168.0.143/sistemaTecnicoApi/api',
-    api: 'http://192.168.0.143'
+    api: 'http://192.168.0.143/sistemaTecnicoApi'
 };
